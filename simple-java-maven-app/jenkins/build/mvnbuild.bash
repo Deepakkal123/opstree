fa05d2161c52e095440104bb1a3b6f6ca27df5a5
Deepak <kalra.deepak94@gmail.com>
2020-09-27 00:06:19 +0530

    file

--- simple-java-maven-app/jenkins/build/mvnbuild.bash
+++ simple-java-maven-app/jenkins/build/mvnbuild.bash
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/home/deepak/jenkins-data/jenkins_home/workspace/CI-pipe
+WORKSPACE=/home/deepak/jenkins/jenkins-data/jenkins_home/workspace/CI-pipe
 
-docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
+docker run --rm  -v  $WORKSPACE/simple-java-maven-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
