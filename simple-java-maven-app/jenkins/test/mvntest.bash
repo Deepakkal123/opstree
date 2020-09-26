219ded3f43eb5c76c766e5cbf2856120ddb7618b
Deepak <kalra.deepak94@gmail.com>
2020-09-27 00:08:12 +0530

    file

--- simple-java-maven-app/jenkins/test/mvntest.bash
+++ simple-java-maven-app/jenkins/test/mvntest.bash
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** testing jar ***********"
 echo "***************************"
 
-WORKSPACE=/home/deepak/jenkins/jenkins-data/jenkins_home/workspace/CI-pipe
+WORKSPACE=/home/deepak/jenkins/jenkins_data/jenkins_home/workspace/CI-pipe
 
 docker run --rm  -v  $WORKSPACE/simple-java-maven-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
