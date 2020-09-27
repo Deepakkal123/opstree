ffc7a987b6982606d0bafd1e577cd653f63def1c
Deepak <kalra.deepak94@gmail.com>
2020-09-27 23:44:01 +0530

    file

--- simple-java-maven-app/jenkins/build/deploy.bash
+++ simple-java-maven-app/jenkins/build/deploy.bash
@@ -3,5 +3,5 @@
 echo "#########Downloading jar from nexus $BUILD_NUMBER ################"
 
 
-curl "http://192.168.0.110:8081/repository/com/com/mycompany/app/my-app/1.0-SNAPSHOT/my-app-1.0-20200927.192113-$BUILD_NUMBER.jar" -o /var/jenkins_home/workspace/CD-CI-pipeline/my-app.jar
+curl "http://192.168.0.110:8081/repository/com/com/mycompany/app/my-app/1.0-SNAPSHOT/my-app-1.0-20200927.234127-$BUILD_NUMBER.jar" -o /var/jenkins_home/workspace/CD-CI-pipeline/my-app.jar
 
