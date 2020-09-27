e0c63d4165fa9aec9581995b95e2a92c730717eb
Deepak <kalra.deepak94@gmail.com>
2020-09-27 23:40:22 +0530

    file

--- simple-java-maven-app/jenkins/build/mvnbuild.bash
+++ simple-java-maven-app/jenkins/build/mvnbuild.bash
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** Building jar ***********"
 echo "***************************"
 
-WORKSPACE=/home/deepak/jenkins/jenkins_data/jenkins_home/workspace/CI-pipe
+WORKSPACE=/home/deepak/jenkins/jenkins_data/jenkins_home/workspace/CI-pipeline
 
 /usr/bin/docker run --rm  -v  $WORKSPACE/simple-java-maven-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
--- simple-java-maven-app/jenkins/test/mvndeploy.bash
+++ simple-java-maven-app/jenkins/test/mvndeploy.bash
@@ -4,6 +4,6 @@ echo "***************************"
 echo "** deploying artifacts to nexus ***********"
 echo "***************************"
 
-WORKSPACE=/home/deepak/jenkins/jenkins_data/jenkins_home/workspace/CI-pipe
+WORKSPACE=/home/deepak/jenkins/jenkins_data/jenkins_home/workspace/CI-pipeline
 
 docker run --rm  -v  $WORKSPACE/simple-java-maven-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
