65b326fc534ca56542690a0978c1633601262b3f
Deepak <kalra.deepak94@gmail.com>
2020-09-28 17:58:30 +0530

    file

--- simple-java-maven-app/jenkins/build/createimage.bash
+++ simple-java-maven-app/jenkins/build/createimage.bash
@@ -4,7 +4,7 @@ echo "***************************"
 echo "rebuilding the image and deploying it"
 echo "***************************"
 
-WORKSPACE=/home/deepak/jenkins/jenkins_data/deploy
+WORKSPACE=/home/deepak/jenkins/jenkins_data/deploy/
 
 docker-compose -f test.yml up -d
 
